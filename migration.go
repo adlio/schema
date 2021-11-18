5c51efb7c5d3ca070350c652f116937a04680090
Aaron Longwell <a@adl.io>
2021-11-18 15:05:56 -0800

    Extract AppliedMigration to file

--- migration.go
+++ migration.go
@@ -2,7 +2,6 @@ package schema
 
 import (
 	"sort"
-	"time"
 )
 
 // Migration is a yet-to-be-run change to the schema
@@ -11,42 +10,10 @@ type Migration struct {
 	Script string
 }
 
-// AppliedMigration is a schema change which was successfully
-// completed
-type AppliedMigration struct {
-	Migration
-	Checksum              string
-	ExecutionTimeInMillis int
-	AppliedAt             time.Time
-}
-
 // SortMigrations sorts a slice of migrations by their IDs
 func SortMigrations(migrations []*Migration) {
 	// Adjust execution order so that we apply by ID
 	sort.Slice(migrations, func(i, j int) bool {
 		return migrations[i].ID < migrations[j].ID
 	})
-}
-
-// GetAppliedMigrations retrieves all already-applied migrations in a map keyed
-// by the migration IDs
-//
-func (m Migrator) GetAppliedMigrations(db Queryer) (applied map[string]*AppliedMigration, err error) {
-	applied = make(map[string]*AppliedMigration)
-	migrations := make([]*AppliedMigration, 0)
-
-	rows, err := db.Query(m.Dialect.SelectSQL(m.QuotedTableName()))
-	if err != nil {
-		return
-	}
-	defer rows.Close()
-	for rows.Next() {
-		migration := AppliedMigration{}
-		err = rows.Scan(&migration.ID, &migration.Checksum, &migration.ExecutionTimeInMillis, &migration.AppliedAt)
-		migrations = append(migrations, &migration)
-	}
-	for _, migration := range migrations {
-		applied[migration.ID] = migration
-	}
-	return applied, err
 }
