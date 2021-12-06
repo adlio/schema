6eb36e3f48a37a9691e8811cd9716d2591e58718
Aaron Longwell <a@adl.io>
2021-12-06 13:56:39 -0800

    Add tests for applyTime.Scan.

--- applied-migration_test.go
+++ applied-migration_test.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"strings"
 	"testing"
+	"time"
 )
 
 func TestGetAppliedMigrations(t *testing.T) {
@@ -81,6 +82,38 @@ func TestGetAppliedMigrationsHasFriendlyScanError(t *testing.T) {
 		_, err = migrator.GetAppliedMigrations(db)
 		if err == nil || !strings.Contains(err.Error(), migrator.TableName) {
 			t.Errorf("Expected an error referencing the schema_migrations table's name, got %s", err)
+		}
+	})
+}
+
+func TestApplyTimeScanner(t *testing.T) {
+	t.Run("Nil", func(t *testing.T) {
+		v := applyTime{}
+		err := v.Scan(nil)
+		if err != nil {
+			t.Error(err)
+		}
+	})
+
+	t.Run("Time In UTC", func(t *testing.T) {
+		v := applyTime{}
+		expected := time.Date(2021, 1, 1, 18, 19, 20, 0, time.UTC)
+		src, _ := time.ParseInLocation("2006-01-02 15:04:05", "2021-01-01 18:19:20", time.UTC)
+		err := v.Scan(src)
+		if err != nil {
+			t.Error(err)
+		}
+		assertZonesMatch(t, time.Now(), v.Value)
+		if expected.Unix() != v.Value.Unix() {
+			t.Errorf("Expected %s, got %s", expected.Format(time.RFC3339), v.Value.Format(time.RFC3339))
+		}
+	})
+
+	t.Run("Invalid String Time", func(t *testing.T) {
+		v := applyTime{}
+		err := v.Scan("2000-13-45 99:45:23")
+		if err == nil {
+			t.Errorf("Expected error scanning invalid time")
 		}
 	})
 }
