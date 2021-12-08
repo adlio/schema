05caecfdec3d6dcaacb954451d939e889e34981b
Aaron Longwell <a@adl.io>
2021-12-08 19:36:01 +0000

    Add expectID and expectScriptMatch helpers

--- file_test.go
+++ file_test.go
@@ -8,12 +8,11 @@ import (
 
 func TestMigrationFromFilePath(t *testing.T) {
 	migration, err := MigrationFromFilePath("./example-migrations/2019-01-01 0900 Create Users.sql")
-	if migration.Script != "CREATE TABLE users (id INTEGER NOT NULL PRIMARY KEY);" {
-		t.Error("Failed to get correct contents of migration")
-	}
 	if err != nil {
 		t.Error(err)
 	}
+	expectID(t, migration, "2019-01-01 0900 Create Users")
+	expectScriptMatch(t, migration, `^CREATE TABLE users`)
 }
 
 func TestMigrationFromFilePathWithInvalidPath(t *testing.T) {
@@ -32,26 +31,18 @@ func TestMigrationFromFile(t *testing.T) {
 	if err != nil {
 		t.Error(err)
 	}
-	if migration.ID != "2019-01-01 0900 Create Users" {
-		t.Errorf("Incorrect ID: %s", migration.ID)
-	}
-	if migration.Script != "CREATE TABLE users (id INTEGER NOT NULL PRIMARY KEY);" {
-		t.Errorf("Incorrect Script: %s", migration.Script)
-	}
+	expectID(t, migration, "2019-01-01 0900 Create Users")
+	expectScriptMatch(t, migration, `^CREATE TABLE users`)
 }
 
 func TestMigrationsFromDirectoryPath(t *testing.T) {
 	migrations, err := MigrationsFromDirectoryPath("./example-migrations")
-	SortMigrations(migrations)
 	if err != nil {
 		t.Error(err)
 	}
-	if migrations[0].ID != "2019-01-01 0900 Create Users" {
-		t.Errorf("Incorrect ID: %s", migrations[0].ID)
-	}
-	if migrations[1].ID != "2019-01-03 1000 Create Affiliates" {
-		t.Errorf("Incorrect ID: %s", migrations[1].ID)
-	}
+	SortMigrations(migrations)
+	expectID(t, migrations[0], "2019-01-01 0900 Create Users")
+	expectID(t, migrations[1], "2019-01-03 1000 Create Affiliates")
 }
 
 func TestMigrationsFromDirectoryPathThrowsErrorForInvalidDirectory(t *testing.T) {
